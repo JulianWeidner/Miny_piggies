3bc4610f3c3f20c0f018bcf0f9d25ed01524df9a
julian.nicholas.weidner <julian.nicholas.weidner@gmail.com>
2019-03-15 01:48:00 -0400

    Writing a new piglet to the sql'works'

--- testing/hash_practice.rb
+++ testing/hash_practice.rb
@@ -1,23 +1,28 @@
 require 'yaml'
 require 'sqlite3'
 
-key = "save"
-goal_target = 200
 
-piggies = Hash.new
+
+@db = SQLite3::Database.open('test.db')
+
+
 
 def create_piglet(piggies, key, goal)
   piglet = Hash.new
   piglet['current'] = 0
   piglet['goal'] = goal
-  piglet['percentage'] = 0
+  piglet['withdraw_percentage'] = 0
+  piglet['deposite_percentage'] = 0
   piglet['locked'] = false
-  piggies[key] = piglet
+  puts piggies
+  if piggies['piggies'] = ""
+    piggies['piggies'] = {key => piglet}
+  elsif piggies['piggies'] != ""
+    piggies['piggies'].merge({key => piglet})
+  end 
+  #new piggies
   piggies
 end
-
-create_piglet(piggies, key, goal_target)
-@db = SQLite3::Database.open('test.db')
 
 def create_account
   email = 'test'
@@ -28,6 +33,12 @@ def create_account
   @db.execute('INSERT INTO accounts(email, password, first_name, last_name, total_val, piggies)
   VALUES(?,?,?,?,?,?);',[email, password, first, last, 0, piggies])
 end
+
+def update_piggies(ruby_hash, email)
+  piggies = ruby_hash.to_yaml
+  puts piggies
+  @db.execute("UPDATE accounts SET piggies = ? WHERE email = ?",[piggies, email])
+end
 #add = create_piglet(piggies,'rent',500)
 #piggies 
 #yam = piggies.to_yaml
@@ -37,17 +48,34 @@ end
 #re_yammed = un_yam.to_yaml
 #print re_yammed
 # 
-def piggies_ruby(account)
+def piggies_to_ruby_hash(account)
   piggies_sql = @db.execute('SELECT piggies FROM accounts where email = ? ',[account])
   piggies_hash = YAML.load(piggies_sql[0][0])
+  piggies_hash
 end 
 
 
 
-def add_piglet
- piggies = @db.execute()
+def add_piglet(email)
+  #get origianl Piggies hash
+  piggies_hash = piggies_to_ruby_hash(email)
+  
+  #get the information of the goal
+  print "\n| Goal Name: "
+  key = gets.chomp.capitalize
+  print "\n| Target Amount: "
+  goal_target = gets.chomp.to_i
+  
+  #create the ruby hash
+  new_piggies_hash = create_piglet(piggies_hash, key, goal_target)
+  update_piggies(new_piggies_hash, email)
+  
+  
+  
+ #piggies = @db.execute()
 end
 
 #load_piggies
 #create_account
-piggies_to_rb('test')
+add_piglet('test')
+add_piglet('test')
