9fb32fbbb64225c193f93d05bb17187953897458
julian.nicholas.weidner <julian.nicholas.weidner@gmail.com>
2019-03-18 22:08:19 -0400

    Cleaned up application.rb

--- lib/application.rb
+++ lib/application.rb
@@ -1,18 +1,20 @@
 require './lib/classes/account.rb'
+#This is the main application. It is the View of the application, and gets the input
+#
 
+#INPUT METHODS
+#they return input at various parts of the application
 def create_test_acc
   user = Account.new('test@testmail.com','test123')
   user.sql_write
 end
   
-
-
 def get_login
   print "| Email: "
   email = gets.chomp.downcase
   print "| Password: "
   password = gets.chomp
-  return email, password
+  return email, password #Tupel example used here, assignment can be found at the top of the main application function
 end
 
 def get_create
@@ -27,12 +29,14 @@ def get_create
   print "| Password Check: "
   pass_check = gets.chomp
   if pass != pass_check
-    exit
+    exit #while loop
   else
     new_user = Account.new(email, pass).create_acc(first_name,last_name)
   end
 end
 
+#VIEW METHODS
+#all these methods will handle view printing, some of them have input returns. I'll write a universal input method shortly
 def startup_view
   view = <<-string
  ____________________________________________
@@ -57,16 +61,16 @@ def startup_view
 |                                            |
 |____________________________________________|
 string
-  
   print view
   print "| INPUT(login,create,help): "
   input = gets.chomp.downcase
 end
 
-  def account_view(user_obj_parm)
-    user = user_obj_parm
-    #puts data
-    view = %(
+def account_view(user_obj_parm)
+  user = user_obj_parm
+  #prints data
+  #write a loop for piglets 
+  view = %(
  ____________________________________________
 |                                            |
 |  Account Value: $#{user.acc_val}                 
@@ -94,42 +98,34 @@ end
 | Transfer To Piglet                         | 
 | Setup Auto Feed                            | 
 )
-    print view
-    print "| INPUT: "
-    gets.chomp.downcase
-  end
+  print view
+  print "| INPUT: "
+  gets.chomp.downcase
+end
 
-
-
- 
+#Main method. The whole application comes together within this method. Change if you dare. 
 def application
   start_input = startup_view
   if start_input == 'login'
     #login
-    email, pass = get_login
+    email, pass = get_login #tupel
+    
     user = Account.new(email, pass)
     user.login
     #show financial and actions
     account_input = account_view(user)
     if account_input == 'create'
           user.add_piglet(user.email)
-      
-     
-        
     end
-    
-    elsif start_input == 'create'
-
-
-    else
+  
+  elsif start_input == 'create'
+  else
     puts "not coded"
     exit
   end
-  
 end
-#call app
-#Sapp 
 
+Actually run application
 application
 
 
